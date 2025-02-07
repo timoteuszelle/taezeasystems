1c1cc1521e1f73d745175db58e36b4dc670c6ae2
Tim Oudesluijs-Zelle <tim@oudesluijszelle.nl>
2025-02-07 09:35:43 +0100

    fix: correct nix trusted-users configuration

--- hosts/sakai/config.nix
+++ hosts/sakai/config.nix
@@ -520,7 +520,8 @@ in
         "flakes"
       ];
       substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
+    trusted-users = [ "root" "tim" ];
     };
     gc = {
       automatic = true;
